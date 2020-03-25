70ddb14db3e20f60a95ba5d81d8416cd707ea409
Sarah Holder <sr.holder@yahoo.com>
2020-03-24 21:59:50 -0500

    deployed app

--- src/javascripts/components/singleFarmer/singleFarmer.js
+++ src/javascripts/components/singleFarmer/singleFarmer.js
@@ -1,12 +1,8 @@
-import farmerData from '../../helpers/data/farmerData';
-import utils from '../../helpers/utils';
 import smash from '../../helpers/data/smash';
+import utils from '../../helpers/utils';
 
 const buildFarmer = (e) => {
   const farmerId = e.target.closest('.card').id;
-  farmerData.getFarmerById(farmerId)
-    .then((response) => {
-      const singleFarmer = response.data;
   smash.getSingleFarmerWithCows(farmerId)
     .then((singleFarmer) => {
       let domString = '';
@@ -15,9 +11,9 @@ const buildFarmer = (e) => {
       domString += '<div class="card text-white bg-dark">';
       domString += `<div class="card-header">Farmer ${singleFarmer.name} (Age: ${singleFarmer.age})</div>`;
       domString += '<div class="card-body">';
-      domString += '<h3 class="card-title">Cows Owned: </h3>';
+      domString += '<h3 class="card-title">Cow(s) Owned:</h3>';
       singleFarmer.cows.forEach((cow) => {
-        domString += `<p>${cow.name} (${cow.location})</p>`;
+        domString += `<p class="card-text">${cow.name} (${cow.location})</p>`;
       });
       domString += '</div>';
       domString += '</div>';
